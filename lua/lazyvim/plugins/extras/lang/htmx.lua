2f5e7153940d8a8e165d513f5b90a3a04ea0300e
Ben O'Mahony <ben@jobsasi.com>
2024-11-15 16:18:31 +0000

    Initial commit for htmx/html

---
+++ lua/lazyvim/plugins/extras/lang/htmx.lua
@@ -0,0 +1,32 @@
+return {
+  recommended = function()
+    return LazyVim.extras.wants({
+      ft = "htmx",
+    })
+  end,
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = { ensure_installed = { "htmx", "html" } },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {},
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    optional = true,
+    opts = function(_, opts)
+      opts.auto_brackets = opts.auto_brackets or {}
+      table.insert(opts.auto_brackets, "htmx")
+    end,
+  },
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        nix = { "htmlbeautifier" },
+      },
+    },
+  },
+}
